fa2b746d418e555d9abb5e8475f5614c4ffae995
root <root@WIN-SNV3MDU7BI1.localdomain>
2021-05-02 13:18:12 +0800

    initial settings and create model, controller

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'order/index'
+  get 'order/new'
+  get 'order/create'
+  get 'order/destroy'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
---
+++ db/migrate/20210502045316_create_order_lines.rb
@@ -0,0 +1,11 @@
+class CreateOrderLines < ActiveRecord::Migration[5.2]
+  def change
+    create_table :order_lines do |t|
+      t.references :order, foreign_key: true
+      t.references :item, foreign_key: true
+      t.integer :amount
+
+      t.timestamps
+    end
+  end
+end
