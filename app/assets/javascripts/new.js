98a99063b356aede865114ed06f98cb5fc73f96b
Alexei Pecherskyi <kegz@mail.ru>
2021-05-03 02:23:59 +0800

    add new view

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -1,7 +1,7 @@
 function addInputGroup() {
 
     var line_block = document.getElementById('line-order');
-
+console.log('1111');
 for (let i=0; i < line_block.children.length;i++){
     line_block.children[i].value = '';
 }
