e66647e644f88516074e4fe23b26ef44d10c5b9d
root <root@WIN-SNV3MDU7BI1.localdomain>
2021-05-06 15:48:53 +0800

    add link to index or new action of order

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  # get 'order/index'
   # get 'order/get_list'
   # get 'order/new'
   # post 'order/create'
   resources :order, only: [:index, :new, :create]
-
+  root 'order#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
